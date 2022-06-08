769ec8be226741b3d80ac2f449ea268d02ee61b6
wilson.ramirez <wilson.ramirez@truextend.com>
2022-06-08 15:59:53 -0400

    Added new actions to get current users and to clear it

--- src/app/store/actions/users-page.actions.ts
+++ src/app/store/actions/users-page.actions.ts
@@ -1,7 +1,9 @@
 import { createAction, props } from "@ngrx/store";
 import { User } from "src/app/models/User";
 
-export const loadUsers = createAction('[Users Action] Load Users');
+export const loadUsers = createAction(
+  '[Users Action] Load Users'
+);
 
 export const loadUsersSuccess = createAction(
   '[Users Action] Users Load Success',
@@ -11,6 +13,15 @@ export const loadUsersSuccess = createAction(
 export const loadUsersFailure = createAction(
   '[Users Action] Users Load Failure',
   props<{ error: string }>()
+);
+
+export const selectUser = createAction(
+  '[Users Action] Select User',
+  props<{userId: number}>()
+);
+
+export const clearSelectedUser = createAction(
+  '[Users Action] Clear Selected User'
 );
 
 export const createUser = createAction(
--- src/app/store/reducers/users-page.reducer.ts
+++ src/app/store/reducers/users-page.reducer.ts
@@ -1,9 +1,10 @@
-import { createReducer, on } from '@ngrx/store';
+import { createReducer, createSelector, on } from '@ngrx/store';
 import { User } from 'src/app/models/User';
 import * as UserActions from '../actions/users-page.actions';
 
 export interface UserState {
     users: User[];
+    activeUserId: number | null;
     error: string;
     status: 'pending' | 'loading' | 'error' | 'success';
 }
@@ -11,6 +12,7 @@ export interface UserState {
 
 export const initialState: UserState = {
     users: [],
+    activeUserId: null,
     error: null,
     status: 'pending'
 }
@@ -32,6 +34,14 @@ export const usersReducer = createReducer(
             username: newUser.username,
             email: newUser.email }],
     })),
+    on(UserActions.selectUser, (state, {userId}) => ({
+        ...state,
+        activeUserId: userId
+    })),
+    on(UserActions.clearSelectedUser, (state) => ({
+        ...state,
+        activeUserId: null
+    })),
     on(UserActions.loadUsers, (state) => ({ ...state, status: 'loading' })),
     on(UserActions.loadUsersSuccess, (state, { users }) => ({
         ...state,
@@ -46,3 +56,13 @@ export const usersReducer = createReducer(
   }))
 )
 
+
+export const selectUsers = (state: UserState) => state.users;
+export const selectActiveUserId = (state: UserState) => state.activeUserId;
+export const selectUserId = createSelector(
+  selectUsers,
+  selectActiveUserId,
+  (users, activeUserId) => {
+      return users.find((user) => user.id === activeUserId) || null;
+  }
+);
